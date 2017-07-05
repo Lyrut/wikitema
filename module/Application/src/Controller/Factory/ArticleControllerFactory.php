1d146eafb95d93f52436af9550b018262c9dba4d
Lyrut <txuj91@gmail.com>
2017-07-05 14:41:59 +0200

    modif article

--- module/Application/src/Controller/Factory/ArticleControllerFactory.php
+++ module/Application/src/Controller/Factory/ArticleControllerFactory.php
@@ -5,6 +5,7 @@ use Interop\Container\ContainerInterface;
 use Zend\Authentication\AuthenticationService;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\Session\SessionManager;
+use Application\Controller\ArticleController;
 
 class ArticleControllerFactory implements FactoryInterface
 {
@@ -13,7 +14,7 @@ class ArticleControllerFactory implements FactoryInterface
         $entityManager = $container->get('doctrine.entitymanager.orm_default');
         $authenticationService = $container->get(AuthenticationService::class);
         $sessionManager = $container->get(SessionManager::class);
-        
+
         // Instantiate the controller and inject dependencies
         return new ArticleController($entityManager,$authenticationService, $sessionManager);
     }
