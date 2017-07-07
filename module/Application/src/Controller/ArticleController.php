d27987f22124e79df1df044029db579ddfeff07d
Axel Bolo <bolo800@gmail.com>
2017-07-07 13:09:13 +0200

    Correction vu utilisateur

--- module/Application/src/Controller/ArticleController.php
+++ module/Application/src/Controller/ArticleController.php
@@ -268,9 +268,26 @@ class ArticleController extends AbstractActionController {
 
         $articles = $this->entityManager->getRepository(Article::class)
                 ->getArticlesByUser($user);
+        
+        foreach($articles as $article)
+        {
+            // Récupère les données de l'utilisateur
+            $idUserArticle = $article->getUser()->getId();
+            $user = $this->entityManager->getRepository(User::class)->find($idUserArticle);
+            $article->setUser($user);
+
+            // Récupère les données des thématiques
+            $idThemeArticle = $article->getTheme()->getId();
+            $theme = $this->entityManager->getRepository(Theme::class)->find($idThemeArticle);
+            $article->setTheme($theme);
+
+            $commentaires[$article->getId()] = $this->entityManager->getRepository(Commentaire::class)
+                    ->getAllCommentairesByArticle($article->getId());
+        }
 
         return new ViewModel([
-            'articles' => $articles
+            'articles' => $articles,
+            'commentaires' => $commentaires
         ]);
     }
     
